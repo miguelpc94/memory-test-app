e085b6dfac1f9141a47865cb700007077d6fcde4
Miguel Castro <miguelpdecastro@gmail.com>
2019-06-01 18:47:32 -0300

    Add files via upload

---
+++ precache-manifest.6b3b77e2a34fbbbc48081d47f3cd7f03.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cab7d57b3e40aac649b813dcc163ed38",
+    "url": "/memory-test-app/index.html"
+  },
+  {
+    "revision": "93974adb319e54882a2b",
+    "url": "/memory-test-app/static/css/main.25c28106.chunk.css"
+  },
+  {
+    "revision": "10bf244ca692441702db",
+    "url": "/memory-test-app/static/js/2.1b3a0643.chunk.js"
+  },
+  {
+    "revision": "93974adb319e54882a2b",
+    "url": "/memory-test-app/static/js/main.b2999139.chunk.js"
+  },
+  {
+    "revision": "a6ee894a087da44839c4",
+    "url": "/memory-test-app/static/js/runtime~main.4be55576.js"
+  }
+]);
